2eff0a159eca4fcfc40dd665220204268207d2dc
ni032mas <ni032mas@mail.ru>
2018-12-05 16:17:57 +0300

    first commit

---
+++ app/src/main/java/ru/smartms/rfidreader_writer/MainActivity.kt
@@ -0,0 +1,19 @@
+package ru.smartms.rfidreader_writer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ru.smartms.rfidreader_writer.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
